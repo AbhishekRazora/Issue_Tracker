a5ee53f0974d17445811a9516a83c7aaa7154e35
Abhishek Razora <abhishekrajora786@gmail.com>
2023-04-15 07:28:28 +0530

    setup static files ,layouts,partials and views

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+require('dotenv').config()
 const express=require("express")
 const app=express()
-const port=6000;
-
+const port=process.env.PORT || 6000;
+const expressLayouts=require('express-ejs-layouts')
+app.use(express.static('./assets'))
+app.use(expressLayouts);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.set('view engine','ejs')
+app.set('views','./views')
+app.use('/', require('./routes'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`)
